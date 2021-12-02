8ea10525b84c568efae64481b1165e23b0462017
dacitto <dacidigital@gmail.com>
2021-12-02 18:57:32 +0100

    feat: add filter to Home page

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,13 +1,14 @@
 import { _getUsers } from "../utils/_DATA.js";
 import { receiveUsers } from "./users";
 import { receiveQuestions } from "./questions";
-
+import { setAuthedUser } from "./authedUser";
 //const AUTHED_ID = "tylermcginnis";
 export const handleInitialData = () => {
   return (dispatch) => {
     console.log(_getUsers());
     return _getUsers().then((users) => {
       dispatch(receiveUsers(users));
+      dispatch(setAuthedUser(JSON.parse(localStorage.getItem("authedUser"))));
     });
   };
 };
