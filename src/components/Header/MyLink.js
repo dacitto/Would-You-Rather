ddba97c20aac9c13f161258941490d4f0de2c45a
dacitto <dacidigital@gmail.com>
2021-12-07 17:33:30 +0100

    fix: setActive navbar link based on pathname

--- src/components/Header/MyLink.js
+++ src/components/Header/MyLink.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 const MyLink = ({ path = "/", name, setActive, active }) => {
   const activeClasses = "font-bold underline";
-  console.log("active");
-  console.log(active);
+
   return (
     <Link
       to={path}
