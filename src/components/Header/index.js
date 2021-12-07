d212fc0f38f593dc22c9262f62b65781fe09755c
dacitto <dacidigital@gmail.com>
2021-12-07 15:16:10 +0100

    feat: add logout  and username to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import MyLink from "./MyLink";
+import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const authedUser = useSelector((state) => state.authedUser);
+  const userInfos = useSelector((state) => state.users[authedUser]);
+  const Navigate = useNavigate();
+  console.log("authedUser");
+  console.log(userInfos);
   const [active, setActive] = useState("/");
   const Links = [
     { path: "/", name: "Home" },
@@ -9,7 +16,7 @@ const Header = () => {
   ];
   return (
     <header className="w-full py-5 bg-indigo-800">
-      <nav className="container mx-auto text-indigo-50 w-full flex justify-center text-lg">
+      <nav className="container px-6 mx-auto text-indigo-50 w-full flex justify-between text-lg">
         <span className="flex gap-4">
           {Links.map((link) => (
             <MyLink
@@ -20,6 +27,18 @@ const Header = () => {
               active={active}
             />
           ))}
+        </span>
+        <span className="flex gap-2">
+          <div>{userInfos.name.split(" ")[0]}</div>
+          <button
+            onClick={() => {
+              localStorage.clear();
+              Navigate("/login");
+            }}
+            className="px-2 rounded-md border font-semibold border-indigo-100 hover:bg-indigo-50 hover:text-indigo-800 transition-all duration-300"
+          >
+            Logout
+          </button>
         </span>
       </nav>
     </header>
